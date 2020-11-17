fc681b859532a1e82848fb7e11fde955aa8c6fca
barendan <barendan@gmail.com>
2020-11-17 18:51:34 -0500

    configure bcrypt and jwt for user creation and auth

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,50 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const { SECRET_KEY } = require('../../config')
+const User = require('../../models/User')
+
+module.exports = {
+    Mutation: {
+        async register(_, 
+            { registerInput: { username,email,password,confirmPassword } }, context, 
+            info
+        ) {
+            // TODO Validate user data
+            // TODO Make sure user doesn't already exist
+            // TODO hash password and create auth token
+            password = await bcrypt.hash(password, 12)
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            })
+
+            const res = await newUser.save()
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, SECRET_KEY, { expiresIn:'1h' })
+
+            return {
+                ...res._doc,
+                id: res._id,
+                token
+            }
+        }
+    },
+    Query: {
+        async getUsers(){
+            try{
+                const users = await User.find();
+                return users
+            } catch (err) {
+                throw new Error(err)
+            }
+        }
+    }
+}
