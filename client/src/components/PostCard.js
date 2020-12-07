366adafd25f964db1648141f9f2cfe36c60562a7
barendan <barendan@gmail.com>
2020-12-07 14:53:51 -0500

    touch up styles

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -5,6 +5,14 @@ import moment from 'moment';
 
 function PostCard({ post: { body, createdAt, id, username, likeCount, commentCount, likes } }) {
 
+  function likePost() {
+    console.log('likePost')
+  }
+
+  function commentOnPost() {
+    console.log('commentOnPost')
+  }
+
   return (
     <Card fluid>
       <Card.Content>
@@ -18,12 +26,20 @@ function PostCard({ post: { body, createdAt, id, username, likeCount, commentCou
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <Button as='div' labelPosition='right'>
+        <Button as='div' labelPosition='right' onClick={likePost}>
           <Button color='teal' basic>
             <Icon name='heart' />
           </Button>
           <Label basic color='teal' pointing='left'>
             {likeCount}
+          </Label>
+        </Button>
+        <Button as='div' labelPosition='right' onClick={commentOnPost}>
+          <Button color='blue' basic>
+            <Icon name='comments' />
+          </Button>
+          <Label basic color='blue' pointing='left'>
+            {commentCount}
           </Label>
         </Button>
       </Card.Content>
