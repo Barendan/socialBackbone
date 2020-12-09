b58c5d242e304d9d863f3796aed771b0875174bd
barendan <barendan@gmail.com>
2020-12-09 17:33:46 -0500

    implement delete comment logic

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -40,31 +40,45 @@ function SinglePost(props) {
             />
           </Grid.Column>
           <Grid.Column width={10}>
-            <Card.Content>
-              <Card.Header>{username}</Card.Header>
-              <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
-              <Card.Description>{body}</Card.Description>
-            </Card.Content>
-            <hr />
-            <Card.Content extra>
-              <LikeButton user={user} post={{ id, likeCount, likes }} />
-              <Button
-                as="div"
-                labelPosition="right"
-                onClick={() => console.log('comment on post')}
-              >
-                <Button basic color="blue">
-                  <Icon name="comments" />
-                </Button>
-                <Label basic color="blue" pointing="left">
-                  {commentCount}
-                </Label>
+            <Card fluid>
+              <Card.Content>
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Description>{body}</Card.Description>
+              </Card.Content>
+              <hr />
+              <Card.Content extra>
+                <LikeButton user={user} post={{ id, likeCount, likes }} />
+                <Button
+                  as="div"
+                  labelPosition="right"
+                  onClick={() => console.log('comment on post')}
+                >
+                  <Button basic color="blue">
+                    <Icon name="comments" />
+                  </Button>
+                  <Label basic color="blue" pointing="left">
+                    {commentCount}
+                  </Label>
 
-              </Button>
-              {user && user.username === username && (
-                <DeleteButton postId={id} callback={deletePostCallback} />
-              )}
-            </Card.Content>
+                </Button>
+                {user && user.username === username && (
+                  <DeleteButton postId={id} callback={deletePostCallback} />
+                )}
+              </Card.Content>
+            </Card>
+            {comments.map(comment => (
+              <Card fluid key={comment.id}>
+                <Card.Content>
+                  {user && user.username === comment.username && (
+                    <DeleteButton postId={id} commentId={comment.id} />
+                  )}
+                  <Card.Header>{comment.username}</Card.Header>
+                  <Card.Meta>{moment(comment.createdAt).fromNow()}</Card.Meta>
+                  <Card.Description>{comment.body}</Card.Description>
+                </Card.Content>
+              </Card>
+            ))}
           </Grid.Column>
         </Grid.Row>
       </Grid>
